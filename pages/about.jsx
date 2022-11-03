e6057db4e404a4bbe2aaea7c5b7b67ebe6840ea3
Levi Oquendo <levi.oquendo@gmail.com>
2022-11-03 01:41:08 -0400

    about fixed

--- pages/about.jsx
+++ pages/about.jsx
@@ -11,8 +11,10 @@ const About = () => {
                     <h2 className={styles.title}>About</h2>
                     <p className={styles.content}> Search every sound from this app to
                         enjoy your favorites podcasts, web show or music whitout ads
-                        Search every sound from this app to
-                        enjoy your favorites podcasts, web show or music whitout ads
+                       
+                        If you're looking for a way to enjoy your favorite podcasts, web shows or music without ads, then look no further than the Search every sound app. This great little app allows you to search for any sound you want, and then play it back without any annoying commercials.
+
+What's even better is that the app is totally free to use. So if you're tired of dealing with ad-filled streaming services, give Search every sound a try today – you won't be disappointed!
                     </p>
                     <footer className={styles.footer}>
                         <img src='Arrow 1.svg' alt="ARROW_SVG" />
