5807069c786590bf86f4d25c14ae6b23a631d5ce
Levi oquendo <54660320+levmetal@users.noreply.github.com>
2022-11-03 23:39:53 -0400

    Update [search].jsx

--- pages/search/[search].jsx
+++ pages/search/[search].jsx
@@ -43,7 +43,7 @@ export async function getServerSideProps(context) {
   const { query } = context
   const { search } = query
   try {
-    const response = await fetch(`http://localhost:3000/api/search/${search}`)
+    const response = await fetch(`https://playersound.vercel.app/api/search/${search}`)
     const data = await response.json()
 
     return {
