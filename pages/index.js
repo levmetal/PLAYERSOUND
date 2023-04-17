7010f9818004aaa93191d58c1aa9b913b284f38a
Levi oquendo <54660320+levmetal@users.noreply.github.com>
2023-04-17 17:34:51 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,18 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Layout from '../components/layout'
-
-
+import Head from 'next/head'
 
 export default function Home() {
-
-
 
   const router = useRouter()
   const [search, setSearch] = useState("")
   const [submiting, setSubmit] = useState(false)
 
-
-
-
   const handleSubmit = (e) => {
     e.preventDefault()
-
     setSubmit(!submiting)
-
-
-
-
     router.push(`/search/${search}`)
-
-
-
     setSearch("")
     setSubmit(!submiting)
   }
@@ -36,6 +22,9 @@ export default function Home() {
   return (
     <>
       <Layout>
+    <Head>
+       <meta name="google-site-verification" content="_Y-WmLWOjBhsxYjfH08TLdnHZ0-SoiKZmeJ9IelQI0g" />
+      </Head>
         <main className={styles.container}>
 
           <>
@@ -52,18 +41,11 @@ export default function Home() {
 
                 <input spellCheck="false" className={styles.form__input} value={search} type="text" placeholder='Lets Rock !' onChange={e => setSearch(e.target.value)} />
                 <button className={styles.form__button} type="submit" >Search</button>
-
-
               </form>
             </div>
             <img className={styles.imghero} src='discHero.png' />
 
           </>
-
-
-
-
-
         </main>
       </Layout>
     </>
